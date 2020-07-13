a3a09f211dbb86d1518f2331394a46019e8eb0a8
Mickael-Roger <33000645+Mickael-Roger@users.noreply.github.com>
2020-07-13 21:40:03 +0200

    Create motors.py

---
+++ motors/motors.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO   
+import time
+
+in1 = 33
+in2 = 40
+ena = 35
+
+in3 = 38
+in4 = 36
+enb = 37
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+
+GPIO.setup(ena,GPIO.OUT)
+GPIO.setup(enb,GPIO.OUT)
+
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
+
+p=GPIO.PWM(ena,1000)
+p=GPIO.PWM(enb,1000)
+
+p.start(50)
+
+GPIO.output(in1,GPIO.HIGH)
+GPIO.output(in2,GPIO.LOW)
+
+time.sleep(10)
+
+GPIO.cleanup()
