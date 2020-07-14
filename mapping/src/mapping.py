0fc4201558fac8519e7df8c228dac8e21b6293f7
Mickael-Roger <33000645+Mickael-Roger@users.noreply.github.com>
2020-07-14 16:53:59 +0200

    Create mapping.py

---
+++ mapping/src/mapping.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import rospy
+from std_msgs.msg import String
+import numpy as np
+import math
+
+
+class Mapping():
+
+    def __init__(self):
+        rospy.init_node('mapping', anonymous=True)
+        self.postion = [1000., 1000.]
+        self.map = np.zeros((2000, 2000))
+
+
+    def start(self):
+        # Listen to Lidar
+        rospy.Subscriber("lidar", String, self.lidarMessage)
+        rospy.spin()
+
+
+    def lidarMessage(self, data):
+        mesure = self.parseMsg(data.data)
+        self.updateMap(mesure)
+        
+        
+    def updateMap(self, mesures):
+        
+        for mesure in mesures:
+            distance = int(mesures[mesure])/10
+            x = self.position[0] + distance
+            y = self.position[1] + math.tan(math.degrees(int(mesure))) * distance
+            self.map[x][y] = 1.
+            
+        print('\n'.join([''.join(['{:2}'.format(item) for item in row]) for row in self.map]))
+        print("------------------------------------")
+            
+        
+    def parseMsg(self, msg):
+        msg=msg.translate({ord(' '): None, ord('{'): None, ord('}'): None, ord('\n'): None})
+        res={}
+
+        for mesure in msg.split(','):
+            val=mesure.split(':')
+            res[val[0]] = int(val[1]) 
+
+        return res
+        
+        
+
+if __name__ == '__main__':
+    try:
+        mapping = Mapping()
+        mapping.start()
+    except rospy.ROSInterruptException:
+        del listen
+        pass
